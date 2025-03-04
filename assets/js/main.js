b1306c7e03d905748ede91505222d1b4bcee0dd2
koraanserbot <104121519+koraanserbot@users.noreply.github.com>
2025-03-03 23:30:36 -0600

    Final Fix - no edits v1

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+ $('.siteBar-btn').click( function (event){ 
+    event.preventDefault()
+    $(this).toggleClass('active');   
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+  // owlCarousel
+ 
+
+  // owlCarousel
+  $(".brand-active").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 6
+      }
+    }
+  });
+
+
+  // page Animation
+  AOS.init({
+    mirror: true,
+    duration: 1500,
+    initClassName: 'aos-init',
+    once: false,
+  });
+
+  // data-aos="fade-up" 
+  // data-aos-delay="300" 
+
+ 
+})(jQuery);
